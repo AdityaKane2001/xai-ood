182358e24766dfc158977b8e3acacb0d9e52f8c9
Aditya Kane <64411306+AdityaKane2001@users.noreply.github.com>
2023-10-22 20:06:40 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torchvision import models
+
+def get_model(model_name, num_classes=10, pretrained=False):
+    if model_name == "resnet18":
+        model = models.resnet18(pretrained="IMAGENET1K_V1" if pretrained else None)
+        model.fc = nn.Linear(in_features=512, out_features=num_classes, bias=True)
+        return model
+    elif model_name == "vitb16":
+        model = model.vit_b_16(pretrained="IMAGENET1K_V1" if pretrained else None)
+        if num_classes != 1000:
+            model.heads.head = nn.Linear(in_features=768, out_features=num_classes, bias=True)
+        return model
+    else:
+        raise ValueError(
+            f"Model not recognized, received `{model_name=}`."
+            f"Expected one of ('resnet18', )"
+        )
