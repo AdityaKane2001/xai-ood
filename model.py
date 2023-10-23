9f7256fe4cb0a10d434adb3edb92f37d1bee3e93
Aditya Kane <64411306+AdityaKane2001@users.noreply.github.com>
2023-10-22 20:08:21 -0400

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ def get_model(model_name, num_classes=10, pretrained=False):
         model.fc = nn.Linear(in_features=512, out_features=num_classes, bias=True)
         return model
     elif model_name == "vitb16":
-        model = model.vit_b_16(pretrained="IMAGENET1K_V1" if pretrained else None)
+        model = models.vit_b_16(pretrained="IMAGENET1K_V1" if pretrained else None)
         if num_classes != 1000:
             model.heads.head = nn.Linear(in_features=768, out_features=num_classes, bias=True)
         return model
